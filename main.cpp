8a151f37dc4a0231818a26d360ef80f45bfcc6c5
AvinashNit <147295103+AvinashNit@users.noreply.github.com>
2023-12-16 20:52:08 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,4 +5,27 @@ class A
     int a;
     public:
     A(int a_):a(a_){cout<<"ctor is called"<<endl;}
+    void print()
+    {
+        cout<<"this is inside A"<<endl;//call to its derived will not happen in case of overload
+    }
+    void show()
+    {
+        cout<<"this is inside A"<<endl;
+    }
+}
+class B:public A{
+    int b;
+    public:
+    B(int a_):A(a_),b(a_){cout<<"ctor of B is called"<<endl;}
+    void print(int a)
+    {
+        cout<<"this is inside B"<<endl;
+
+    }
+    void show()
+    {
+        cout<<"this is inside B"<<endl;//base class overridden cannot invoke A::show()
+    }
+
 }
